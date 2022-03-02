952ac10b60f38c6e73b9e14f44d91a51b2cd2029
MajdHarbb <majdwalidharb@gmail.com>
2022-03-02 18:18:53 +0200

    user data on home page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,7 +1,32 @@
 import React from 'react';
 import NavbarComponent from '../components/NavbarComponent';
 import {Outlet} from 'react-router-dom';
+import { useState } from 'react';
+
 const LandingPage = () => {
+    const [first_name,setFirstName] = useState('');
+    const [last_name,setLastName] = useState('');
+    const [email,setEmail] = useState('');
+    const bearer_token = localStorage.getItem("access_token");
+    console.log(bearer_token)
+    var url = "http://127.0.0.1:8000/api/auth/user-profile";
+    var bearer = 'Bearer ' + bearer_token;
+    async function getUserInfo ()  {
+        const response = await fetch(url, {
+            method: 'GET',
+    
+            headers: {
+                'Authorization': bearer,
+                'Content-Type': 'application/json'
+            }
+        })
+        let content = await response.json();
+        console.log(content);
+        setFirstName(content.first_name);
+        setLastName(content.last_name);
+        
+    }
+    getUserInfo();
     return (
         <div>
             <NavbarComponent/>
