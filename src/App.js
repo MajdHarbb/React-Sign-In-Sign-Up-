131b2159ce2a52c4740889fb3b92e85a511fa41d
MajdHarbb <majdwalidharb@gmail.com>
2022-03-02 05:47:42 +0200

    sign up component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import Login from './components/Login';
+import LoginComponent from './components/LoginComponent';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+        <LoginPage/>
     </div>
   );
 }
